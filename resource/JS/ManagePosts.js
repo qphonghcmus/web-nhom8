bb9017c0a6fc886dff386818c938f2e36a2065a5
Lê Hoàng Sang <1612557@student.hcmus.edu.vn>
2019-04-30 02:32:47 +0700

    upload js file for ManagePosts

---
+++ resource/JS/ManagePosts.js
@@ -0,0 +1,38 @@
+$(document).ready( function () {
+    $('#main-table').DataTable(
+    {
+        "oLanguage": 
+        {
+          "sSearch": "Tìm kiếm",
+          "sLengthMenu": "Hiển thị _MENU_ kết quả",
+          "sZeroRecords": "Không tìm thấy kết quả nào",
+          "sProcessing": "Đang tìm kiếm...",
+          "sInfo": "Hiển thị từ _START_ đến _END_ của _TOTAL_ kết quả",
+          "sInfoFiltered": "(Lọc từ _MAX_ bộ)",
+          "sInfoEmpty":      "Không tìm thấy dòng nào phù hợp",
+          "oPaginate": 
+          {
+            "sNext": "Trang kế",
+            "sPrevious": "Trang trước"
+          }
+
+        },
+        "columnDefs": [ {
+            "targets": 2,
+            "orderable": false
+            },{
+                "targets": 3,
+                "orderable": false
+                } ]
+    }
+    );
+
+   
+    
+    $('.dataTables_filter label input').addClass('form-control');
+    $('.dataTables_filter label input').addClass('custom-form-control-search');
+    
+
+   
+    $('.dataTables_length label select').addClass('form-control custom-form-control-select');
+} );
