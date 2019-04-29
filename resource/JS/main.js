fb48d1f0187522266c9c8b09863545c3e23f1e74
lephamhoangquy <hoangquy103@gmail.com>
2019-04-29 11:38:07 +0700

    add shrink navbar on scroll

--- resource/JS/main.js
+++ resource/JS/main.js
@@ -31,3 +31,16 @@ function opentab_(evt, category_) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen_").click();
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+            
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "5px 10px";
+    document.getElementById("logo").style.width = "25%";
+  } else {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.width = "28%";
+  }
+}
