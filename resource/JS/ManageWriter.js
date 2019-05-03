0e2f8f912ac72d21c719c40103a5d3603b1c4099
Lê Hoàng Sang <1612557@student.hcmus.edu.vn>
2019-05-03 21:11:47 +0700

    update Manage Writer

---
+++ resource/JS/ManageWriter.js
@@ -0,0 +1,55 @@
+$(document).ready( function () 
+{
+    $('#main-table').DataTable(
+    {
+        "oLanguage": 
+        {
+          "sSearch": "Tìm kiếm",
+          "sLengthMenu": "Hiển thị _MENU_ kết quả",
+          "sZeroRecords": "Không tìm thấy kết quả nào",
+          "sProcessing": "Đang tìm kiếm...",
+          "sInfo": "Hiển thị từ _START_ đến _END_ của _TOTAL_ kết quả",
+          "sInfoFiltered": "",
+          "sInfoEmpty":      "Không tìm thấy dòng nào phù hợp",
+          "oPaginate": 
+          {
+            "sNext": "Trang kế",
+            "sPrevious": "Trang trước"
+          }
+
+        },
+        "columnDefs": [ {
+            "targets": 3,
+            "orderable": false
+            },{
+                "targets": 4,
+                "orderable": false
+                }]
+    }
+    );
+
+   
+    
+    $('.dataTables_filter label input').addClass('form-control');
+    $('.dataTables_filter label input').addClass('custom-form-control-search');
+    
+
+   
+    $('.dataTables_length label select').addClass('form-control custom-form-control-select');
+
+
+    $("#toggle-add-form").click(function(){
+      $('#add-subscriber-main').delay(1000).toggle();
+       
+   
+    });
+
+    $("#toggle-list-subscriber").click(function(){
+      $('#list-subscriber-main').delay(1000).toggle();
+       
+   
+    });
+  } 
+  );
+
+
