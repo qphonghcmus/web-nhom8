860f300b94294877ac3442b7e69a18576055f1ea
Quoc Phong <1612498@student.hcmus.edu.vn>
2019-05-29 17:11:23 +0700

    update something

--- src/routes/Editor/route.editor.js
+++ src/routes/Editor/route.editor.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.render('./layouts/Editor/editor.ejs',{
+    res.render('./layouts/Editor/main',{
         filename: '../../editor/editor_admin',
         activeAdmin: true,
         cssfiles:[],
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/admin', (req, res) => {
-    res.render('./layouts/Editor/editor.ejs',{
+    res.render('./layouts/Editor/main',{
         filename: '../../editor/editor_admin',
         activeAdmin: true,
         cssfiles:[],
@@ -23,7 +23,7 @@ router.get('/admin', (req, res) => {
 });
 
 router.get('/update', (req, res) => {
-    res.render('./layouts/Editor/editor.ejs',{
+    res.render('./layouts/Editor/main',{
         filename: '../../editor/editor_updateProfile.ejs',
         activeUpdate: true,
         cssfiles:[],
@@ -32,29 +32,35 @@ router.get('/update', (req, res) => {
 });
 
 router.get('/approve', (req, res) => {
-    res.render('./layouts/Editor/editor.ejs',{
+    res.render('./layouts/Editor/main',{
         filename: '../../editor/editor_approved.ejs',
         activeApprove: true,
-        cssfiles:['style-writer-published'],
+        cssfiles:[
+            '../../../public/resource/CSS/style-writer-published.css'
+        ],
         jsfiles:[],
     });
 });
 
 router.get('/reject', (req, res) => {
-    res.render('./layouts/Editor/editor.ejs',{
+    res.render('./layouts/Editor/main',{
         filename: '../../editor/editor_rejected.ejs',
         activeReject: true,
-        cssfiles: ['style-writer-published'],
+        cssfiles: [
+            '../../../public/resource/CSS/style-writer-published.css'],
         jsfiles:[],
     });
 });
 
 router.get('/wait', (req, res) => {
-    res.render('./layouts/Editor/editor.ejs',{
+    res.render('./layouts/Editor/main',{
         filename: '../../editor/editor_waiting.ejs',
         activeWait: true,
-        cssfiles: ['style-writer-published','style-editor-waiting'],
-        jsfiles:['editor_waiting'],
+        cssfiles: [
+            '../../../public/resource/CSS/style-writer-published.css',
+            '../../../public/resource/CSS/style-editor-waiting.css'],
+        jsfiles:[
+            '../../../public/resource/js/editor_waiting.js'],
     });
 });
 
--- src/routes/Writer/route.writer.js
+++ src/routes/Writer/route.writer.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.render('./layouts/Writer/writer.ejs',{
+    res.render('./layouts/Writer/main',{
         filename: '../../writer/writer_admin',
         activeAdmin: true,
         cssfiles:[],
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/admin', (req, res) => {
-    res.render('./layouts/Writer/writer.ejs',{
+    res.render('./layouts/Writer/main',{
         filename: '../../writer/writer_admin',
         activeAdmin: true,
         cssfiles:[],
@@ -22,7 +22,7 @@ router.get('/admin', (req, res) => {
 });
 
 router.get('/update', (req, res) => {
-    res.render('./layouts/Writer/writer.ejs',{
+    res.render('./layouts/Writer/main',{
         filename: '../../writer/writer_update',
         activeUpdate: true,
         cssfiles:[],
@@ -31,64 +31,79 @@ router.get('/update', (req, res) => {
 });
 
 router.get('/post', (req, res) => {
-    res.render('./layouts/Writer/writer.ejs',{
+    res.render('./layouts/Writer/main',{
         filename: '../../writer/writer_post.ejs',
         activePost: true,
-        cssfiles:['style-writer-post'],
+        cssfiles:[
+            '../../../public/resource/CSS/style-writer-post.css'
+        ],
         jsfiles:[],
     });
 });
 
 router.get('/waitPublish', (req, res) => {
-    res.render('./layouts/Writer/writer.ejs',{
+    res.render('./layouts/Writer/main',{
         filename: '../../writer/writer_waitPublish.ejs',
         activeWaitPublish: true,
-        cssfiles:['style-writer-published'],
+        cssfiles:[
+            '../../../public/resource/CSS/style-writer-published.css'
+        ],
         jsfiles:[],
     });
 });
 
 router.get('/published', (req, res) => {
-    res.render('./layouts/Writer/writer.ejs',{
+    res.render('./layouts/Writer/main',{
         filename: '../../writer/writer_published.ejs',
         activePublish: true,
-        cssfiles:['style-writer-published'],
+        cssfiles:[
+            '../../../public/resource/CSS/style-writer-published.css'
+        ],
         jsfiles:[],
     });
 });
 
 router.get('/rejected', (req, res) => {
-    res.render('./layouts/Writer/writer.ejs',{
+    res.render('./layouts/Writer/main',{
         filename: '../../writer/writer_rejected.ejs',
         activeReject: true,
-        cssfiles:['style-writer-published'],
+        cssfiles:[
+            '../../../public/resource/CSS/style-writer-published.css'
+        ],
         jsfiles:[],
     });
 });
 
 router.get('/waitReview', (req, res) => {
-    res.render('./layouts/Writer/writer.ejs',{
+    res.render('./layouts/Writer/main',{
         filename: '../../writer/writer_waitReview.ejs',
         activeWaitReview: true,
-        cssfiles:['style-writer-published'],
+        cssfiles:[
+            '../../../public/resource/CSS/style-writer-published.css'
+        ],
         jsfiles:[],
     });
 });
 
 router.get('/edit', (req, res) => {
-    res.render('./layouts/Writer/writer.ejs',{
+    res.render('./layouts/Writer/main',{
         filename: '../../writer/writer_edit.ejs',
         activeEdit: true,
-        cssfiles:['style-writer-published'],
-        jsfiles:['writer-edit'],
+        cssfiles:[
+            '../../../public/resource/CSS/style-writer-published.css'
+        ],
+        jsfiles:[
+            '../../../public/resource/js/writer-edit.js'],
     });
 });
 
 router.get('/editPage', (req, res) => {
-    res.render('./layouts/Writer/writer.ejs',{
+    res.render('./layouts/Writer/main',{
         filename: '../../writer/writer_editPage.ejs',
-        activePost: true,
-        cssfiles:['style-write-edit'],
+        activeEditPage: true,
+        cssfiles:[
+            '../../../public/resource/CSS/style-write-edit.css'
+        ],
         jsfiles:[],
     });
 });
