1577032bfe2a92dcca75947d6d8b119f71abebbf
Lê Hoàng Sang <1612557@student.hcmus.edu.vn>
2019-06-03 21:44:24 +0700

    update login

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.use('/', require('./routes/TrangChu/index'));
+app.use('/login',require('./routes/TrangDangNhap/route.login'));
 app.use('/list',require('./routes/DanhSachBaiViet/route.dsbaiviet'));
 app.use('/administrator',require('./routes/Admin/route.admin'));
 
