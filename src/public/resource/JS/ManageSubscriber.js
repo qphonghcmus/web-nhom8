a25cdc7ac976883cc49ec3c44b8382d786a18bd7
Le Hoang Sang <1612557@student.hcmus.edu.vn>
2019-06-17 23:25:25 +0700

    tạo bảng danh sách chờ duyệt
    tạo bảng danh sách chờ duyệt trong mục quản lý độc giả của admin

--- src/public/resource/JS/ManageSubscriber.js
+++ src/public/resource/JS/ManageSubscriber.js
@@ -1,6 +1,6 @@
 $(document).ready( function () 
 {
-    $('#main-table').DataTable(
+    $('#listChoDuyet').DataTable(
     {
         "oLanguage": 
         {
@@ -19,36 +19,72 @@ $(document).ready( function ()
 
         },
         "columnDefs": [ {
-            "targets": 4,
+            "targets": 0,
             "orderable": false
             },{
-                "targets": 5,
+                "targets": 1,
                 "orderable": false
-                }]
+                },{
+                  "targets": 2,
+                  "orderable": false
+                  },{
+                    "targets": 3,
+                    "orderable": false
+                    },{
+                      "targets": 4,
+                      "orderable": false
+                      }]
     }
     );
 
-   
+    $('#listDocGia').DataTable(
+      {
+          "oLanguage": 
+          {
+            "sSearch": "Tìm kiếm",
+            "sLengthMenu": "Hiển thị _MENU_ kết quả",
+            "sZeroRecords": "Không tìm thấy kết quả nào",
+            "sProcessing": "Đang tìm kiếm...",
+            "sInfo": "Hiển thị từ _START_ đến _END_ của _TOTAL_ kết quả",
+            "sInfoFiltered": "",
+            "sInfoEmpty":      "Không tìm thấy dòng nào phù hợp",
+            "oPaginate": 
+            {
+              "sNext": "Trang kế",
+              "sPrevious": "Trang trước"
+            }
+  
+          },
+          "columnDefs": [ {
+            "targets": 0,
+            "orderable": false
+            },{
+                "targets": 1,
+                "orderable": false
+                },{
+                  "targets": 2,
+                  "orderable": false
+                  },{
+                    "targets": 3,
+                    "orderable": false
+                    },{
+                      "targets": 4,
+                      "orderable": false
+                      }]
+      }
+      );
     
     $('.dataTables_filter label input').addClass('form-control');
     $('.dataTables_filter label input').addClass('custom-form-control-search');
-    
-
-   
+  
+  
+  
     $('.dataTables_length label select').addClass('form-control custom-form-control-select');
-
-
-    $("#toggle-add-form").click(function(){
-      $('#add-subscriber-main').delay(1000).toggle();
-       
-   
+  
+    $(".content-box-tool a").click(function () {
+      $(this).parent().parent().next().fadeToggle();
     });
-
-    $("#toggle-list-subscriber").click(function(){
-      $('#list-subscriber-main').delay(1000).toggle();
-       
-   
-    });
+  
   } 
   );
 
